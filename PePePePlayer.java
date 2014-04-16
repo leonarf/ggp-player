39c8e5c9751db32ba9f630bf1d822f72e5d4072f
leonarf <leonard.michelet@gmail.com>
2014-04-16 16:09:07 +0200

    Update PePePePlayer.java
    implémentation du minimax bounded dans PePePePlayer

--- PePePePlayer.java
+++ PePePePlayer.java
@@ -1,3 +1,5 @@
+
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -37,47 +39,107 @@ public class PePePePlayer extends StateMachineGamer {
 			GoalDefinitionException
 	{
 		long start = System.currentTimeMillis();
-
-
 		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
-		Move selection = moves.get(0);
-		int maxScore = 0;
-		for( int i=0; i < moves.size(); ++i)
+		Move selection = null;
+		if(getStateMachine().getRoles().size()==1)
 		{
-			int tmpMaxScore = getMaxScore(moves.get(i), getCurrentState());
+			selection = getMoveAccordingCompulsiveDeliberation(moves);
+		}
+		else if(getStateMachine().getRoles().size()==2)
+		{
+			selection = getMiniMaxMove(moves);
+		}
+		long stop = System.currentTimeMillis();
+
+		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		return selection;
+	}
+
+	/*
+	 * Parcours l'arbre du jeu en profondeur et renvoi le mouvement correspondant au score maximal qui peut être obtenu à la fin
+	 * Ne marche que pour les jeux à un seul joueur et qui dont l'arbre peut être entièrement parcouru en 100ms
+	public Move getMoveAccordingCompulsiveDeliberation(List<Move> legalMoves)
+			throws MoveDefinitionException, GoalDefinitionException, TransitionDefinitionException
+	{
+		Move selection = legalMoves.get(0);
+		int maxScore = 0;
+		for( int i=0; i < legalMoves.size(); ++i)
+		{
+			int tmpMaxScore = getMaxScore(legalMoves.get(i), getCurrentState());
 			if( tmpMaxScore == 100)
 			{
-				selection = moves.get(i);
+				selection = legalMoves.get(i);
 				break;
 			}
 			else if(tmpMaxScore > maxScore)
 			{
 				maxScore = tmpMaxScore;
-				selection = moves.get(i);
+				selection = legalMoves.get(i);
 			}
 		}
-
-		long stop = System.currentTimeMillis();
-
-		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
-		return selection;		// TODO Auto-generated method stub
+		return selection;
 	}
+	 */
 
-	public int getMaxScore(Move move, MachineState currentState)
-			throws TransitionDefinitionException, GoalDefinitionException, MoveDefinitionException
+	public Move getMiniMaxMove(List<Move> legalMoves)
+			throws MoveDefinitionException, GoalDefinitionException, TransitionDefinitionException
 	{
-		int maxScore = 0;
-		List<Move> moveList = Arrays.asList(move);
-		MachineState nextState = getStateMachine().getNextState(currentState, moveList);
-		if(getStateMachine().isTerminal(nextState))
-		{
-			return getStateMachine().getGoal(nextState, getRole());
-		}
-
-		List<Move> legalMoves = getStateMachine().getLegalMoves(nextState, getRole());
+		Move selection = legalMoves.get(0);
+		int miniMaxScore = 0;
 		for( int i=0; i < legalMoves.size(); ++i)
 		{
-			int tmpMaxScore = getMaxScore(legalMoves.get(i), nextState);
+			List<Move> moveList = Arrays.asList(legalMoves.get(i));
+			int tmpMaxScore = getMinScore(moveList, getCurrentState(), 1);
+			if( tmpMaxScore == 100)
+			{
+				selection = legalMoves.get(i);
+				break;
+			}
+			else if(tmpMaxScore > miniMaxScore)
+			{
+				miniMaxScore = tmpMaxScore;
+				selection = legalMoves.get(i);
+			}
+		}
+		return selection;
+	}
+
+	public int getMinScore(List<Move> playersMoves, MachineState currentState, int role)
+			throws TransitionDefinitionException, GoalDefinitionException, MoveDefinitionException
+	{
+		int minScore = 0;
+		List<Move> legalEnemyMoves = getStateMachine().getLegalMoves(currentState, getStateMachine().getRoles().get(role));
+		for( int i=0; i < legalEnemyMoves.size(); ++i)
+		{
+			playersMoves.add(legalEnemyMoves.get(i));
+			MachineState nextState = getStateMachine().getNextState(currentState, playersMoves);
+			int tmpMinScore = getMaxScore(nextState);
+			if(tmpMinScore == 0)
+			{
+				return 0;
+			}
+			else if(tmpMinScore < minScore)
+			{
+				minScore = tmpMinScore;
+			}
+		}
+		return minScore;
+	}
+
+	public int getMaxScore(MachineState currentState)
+			throws TransitionDefinitionException, GoalDefinitionException, MoveDefinitionException
+	{
+		if(getStateMachine().isTerminal(currentState))
+		{
+			return getStateMachine().getGoal(currentState, getRole());
+		}
+
+		int maxScore = 0;
+		List<Move> legalMoves = getStateMachine().getLegalMoves(currentState, getRole());
+		for( int i=0; i < legalMoves.size(); ++i)
+		{
+			List<Move> moveList = Arrays.asList(legalMoves.get(i));
+			int tmpMaxScore = getMinScore(moveList, currentState, moveList.size());
 			if(tmpMaxScore == 100)
 			{
 				return 100;
