ab8b17f79c6a5ac0f0871d599e8634d19810cc5b
leonarf <leonard.michelet@gmail.com>
2014-05-07 16:46:21 +0200

    Create EnemyMove.java
    Class to link a move of our player to a new StoredState through a list of move for our enemies

---
+++ EnemyMove.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+import org.ggp.base.util.statemachine.Move;
+
+
+public class EnemyMove {
+	List<Move> mJointMoves;
+	private MyMove mParent;
+	private StoredState mChild;
+}
