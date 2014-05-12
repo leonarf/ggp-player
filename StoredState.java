0814ce37eca55dc06d38e2ff13666116f7fda383
leonarf <leonard.michelet@gmail.com>
2014-05-12 16:58:16 +0200

    Update StoredState.java

--- StoredState.java
+++ StoredState.java
@@ -10,49 +10,70 @@ import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 public class StoredState {
     public StoredState(MachineState state, StateMachine machine, Role role, EnemyMove parent) throws MoveDefinitionException
     {
-	mGameState = state;
-	mChildren = null;
-	mMyRole = role;
-	mMachine = machine;
-	mParent = parent;
+		mGameState = state;
+		mChildren = null;
+		mMyRole = role;
+		mMachine = machine;
+		mParent = parent;
+		mVisitCount = 0;
+		mTotalScore = 0;
     }
 
     public MachineState getState()
     {
-	return mGameState;
+    	return mGameState;
     }
 
     public List<MyMove> getMyLegalMoves() throws MoveDefinitionException
     {
-	if (mChildren == null)
-	{
-	    List<Move> legalMoves = mMachine.getLegalMoves(mGameState, mMyRole);
-	    mChildren = new ArrayList<MyMove>(legalMoves.size());
-	    for (Move move : legalMoves)
-	    {
-		mChildren.add(new MyMove(move, this));
-	    }
-	}
-	return mChildren;
+		if (mChildren == null)
+		{
+		    List<Move> legalMoves = mMachine.getLegalMoves(mGameState, mMyRole);
+		    mChildren = new ArrayList<MyMove>(legalMoves.size());
+		    for (Move move : legalMoves)
+		    {
+			mChildren.add(new MyMove(move, this));
+		    }
+		}
+		return mChildren;
     }
 
     public Role getRole()
     {
-	return mMyRole;
+    	return mMyRole;
     }
 
     public StateMachine getMachine()
     {
-	return mMachine;
+    	return mMachine;
     }
 
     public void deleteParent()
     {
-	mParent = null;
+    	mParent = null;
     }
 
-    int mVisitCount;
-    int mTotalScore;
+    public void UpdateScore(int score)
+    {
+    	++mVisitCount;
+    	mTotalScore += score;
+    	if(mParent != null)
+    	{
+    		mParent.getMyMove().getState().UpdateScore(score);
+    	}
+    }
+
+    public int getScore()
+    {
+    	if(mVisitCount == 0 || mTotalScore == 0)
+    	{
+    		return 0;
+    	}
+    	return mTotalScore/mVisitCount;
+    }
+
+    private int mVisitCount;
+    private int mTotalScore;
     private MachineState mGameState;
     private List<MyMove> mChildren;
 
