e5c53318c07cddb1f22bad68d427aac60459b143
leonarf <leonard.michelet@gmail.com>
2014-05-13 17:04:34 +0200

    Update MyMove.java

--- MyMove.java
+++ MyMove.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
 public class MyMove {
     public MyMove(Move move, StoredState parent) throws MoveDefinitionException
@@ -10,6 +13,7 @@ public class MyMove {
 	mLegalMove = move;
 	mParent = parent;
 	mChildren = null;
+	mVisitCount = 0;
     }
 
     public Move getMove()
@@ -26,18 +30,59 @@ public class MyMove {
     {
 	if (mChildren == null)
 	{
-	    List<List<Move>> legalJointMoves = mParent.getMachine().getLegalJointMoves(mParent.getState(),
-		    mParent.getRole(), mLegalMove);
+	    calculateEnemyMoves();
+	}
+	return mChildren;
+    }
+
+    public int getVisitCount()
+    {
+    	return mVisitCount;
+    }
+
+    public int sendProbe(int[] depth) throws GoalDefinitionException, TransitionDefinitionException, MoveDefinitionException
+    {
+    	++mVisitCount;
+		List<Integer> choices = null;
+		int choosenMove = 0;
+		//Des mouvements n'ont jamais été explorés
+		if(mChildren == null)
+		{
+			calculateEnemyMoves();
+		}
+		if( mVisitCount < mChildren.size())
+		{
+			choices = new ArrayList<>(mChildren.size() - mVisitCount);
+			for(int i=0; i < mChildren.size(); ++i)
+			{
+				if(mChildren.get(i).getVisitCount() == 0)
+				{
+					choices.add(i);
+				}
+			}
+			choosenMove = choices.get(new Random().nextInt(choices.size()));
+		}
+		//Tous les mouvements ont été explorés au moins une fois
+		else
+		{
+			choosenMove = new Random().nextInt(mChildren.size());
+		}
+	    return mChildren.get(choosenMove).getNextState().sendProbe(depth);
+    }
+
+    private void calculateEnemyMoves() throws MoveDefinitionException
+    {
+    	List<List<Move>> legalJointMoves = mParent.getMachine().getLegalJointMoves(mParent.getState(),
+    		    mParent.getRole(), mLegalMove);
 	    mChildren = new ArrayList<EnemyMove>(legalJointMoves.size());
 	    for (List<Move> joinMove : legalJointMoves)
 	    {
 		mChildren.add(new EnemyMove(joinMove, this));
 	    }
-	}
-	return mChildren;
     }
 
     private Move mLegalMove;
     private StoredState mParent;
     private List<EnemyMove> mChildren;
+    private int mVisitCount;
 }
